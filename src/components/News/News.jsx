3eb5fb66beb08d614e23dfd6f63de7c3689b694f
AntoniProk <antoniprok@gmail.com>
2022-06-08 15:35:27 +0300

    git check

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,7 +3,10 @@ import s from './News.module.css'
 let News = () => {
   return (
     <div>
-      <p className={s.title}> привет, Новоснове окно </p>
+      <p className={s.title}>
+        {' '}
+        привет, Новоснове окно дополнение для проветки работы с Git-ом
+      </p>
     </div>
   )
 }
